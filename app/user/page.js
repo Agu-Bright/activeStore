7276d60b7a959365c260404a2478a9d5324e1f22
Bright <brightagu2@gmail.com>
2024-07-18 03:49:23 +0100

    update

--- app/user/page.js
+++ app/user/page.js
@@ -173,8 +173,10 @@ export default function Home() {
             title={<Topic title="Facebook" src="/img/facebook-1.png" />}
           />
           <TableList title={<Topic title="Twitter" src="/img/twitter.png" />} />
-          <TableList title={<Topic title="Instagram" src="/img/star.png" />} />
-          <TableList title={<Topic title="Email" src="/img/star.png" />} />
+          <TableList
+            title={<Topic title="Instagram" src="/img/instagram.png" />}
+          />
+          <TableList title={<Topic title="Email" src="/img/gmail.png" />} />
           <TableList title={<Topic title="Others" src="/img/star.png" />} />
         </Box>
       </NavPage>
