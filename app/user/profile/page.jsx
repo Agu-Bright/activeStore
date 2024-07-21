a6c31265881aeffbabe599f9b760ae941569fd04
Bright <brightagu2@gmail.com>
2024-07-21 06:47:12 +0100

    update

--- app/user/profile/page.jsx
+++ app/user/profile/page.jsx
@@ -15,76 +15,14 @@ import axios from "axios";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import BasicModal from "./UpdateModal";
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
-  const [active, setActive] = useState("popular");
-  const [movies, setMovies] = useState();
 
-  const getRandomMovies = (movies, count) => {
-    const shuffled = movies.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, count);
-  };
-
-  useEffect(() => {
-    const TMDB_API_KEY = " d70595ef3e351a97e5665f2de45fcd45";
-    const TMDB_ACCESS_TOKEN =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzA1OTVlZjNlMzUxYTk3ZTU2NjVmMmRlNDVmY2Q0NSIsInN1YiI6IjY2NzA0MWQ0ODRmMDE5YTBlYWExYjcxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-M4AqRw0LtobEQ4YQ-eAxJxaSz8ym__7cLqyry-7Uvk";
-    const fetchMovies = async () => {
-      try {
-        const { data } = await axios.get(
-          `https://api.themoviedb.org/3/movie/${active}`,
-          {
-            headers: {
-              Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
-            },
-            params: {
-              api_key: TMDB_API_KEY,
-              // sort_by: "popularity.desc", // You can modify this to fetch movies based on different criteria
-              page: 1, // Fetch the first page of results
-            },
-          }
-        );
-
-        const randomMovies = getRandomMovies(data?.results, 12);
-        const movieData = randomMovies.map((movie) => ({
-          name: movie.title,
-          image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-          rating: movie.vote_average,
-        }));
-        setMovies(movieData);
-      } catch (error) {
-        console.error("Error fetching movies:", error);
-      }
-    };
-
-    fetchMovies();
-  }, [active]);
-
-  const [bronzV, setBronzV] = useState(true);
-  const [silverV, setSilverV] = useState(true);
-  const [goldV, setGoldV] = useState(true);
-  const [diamondV, setDiamondV] = useState(true);
-
-  const handleToggle = (type) => {
-    if (type === "bronze") {
-      setBronzV((prev) => !prev);
-      return;
-    }
-    if (type === "silver") {
-      setSilverV((prev) => !prev);
-      return;
-    }
-    if (type === "gold") {
-      setGoldV((prev) => !prev);
-      return;
-    }
-    if (type === "diamond") {
-      setDiamondV((prev) => !prev);
-      return;
-    }
-  };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
 
   if (status === "loading") {
     return (
@@ -141,7 +79,7 @@ export default function Home() {
             </div>
 
             <div className="d-flex justify-content-center mt-3">
-               <strong>{session?.user?.username} </strong> 
+              <strong>{session?.user?.username} </strong>
             </div>
 
             <div className="d-flex justify-content-center mt-1">
@@ -225,9 +163,12 @@ export default function Home() {
 
                   <div className="lh-100">
                     <h6 className="mb-0 text-black lh-100">Change Password</h6>
-                    <a href="change-password">
+                    <div
+                      style={{ cursor: "pointer" }}
+                      onClick={() => setOpen(true)}
+                    >
                       <small>Click to change your password</small>
-                    </a>
+                    </div>
                   </div>
                 </div>
 
@@ -280,6 +221,12 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <BasicModal
+            open={open}
+            handleOpen={handleOpen}
+            setOpen={setOpen}
+            title="Update Password"
+          />
         </div>
       </NavPage>
     );
