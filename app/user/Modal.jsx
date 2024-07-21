48abf0ef2838c88d8216c9c6593000786c986587
Bright <brightagu2@gmail.com>
2024-07-21 06:10:13 +0100

    update

--- app/user/Modal.jsx
+++ app/user/Modal.jsx
@@ -19,7 +19,7 @@ const style = {
 };
 
 export default function BasicModal() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
