39e9b0380c8cb1b16f007b133d53f56060bee7e5
Bright <brightagu2@gmail.com>
2024-08-11 16:15:09 +0100

    update

--- app/api/logs/getCategories/route.js
+++ app/api/logs/getCategories/route.js
@@ -1,17 +1,38 @@
+import { authOptions } from "@app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import connectDB from "@utils/connectDB";
+import { NextResponse } from "next/server";
 import Category from "@models/Category";
 
-export const GET = async (req, res) => {
+export const GET = async (req) => {
+  //check if user is authenticated
+  const session = await getServerSession(
+    req,
+    {
+      ...NextResponse,
+      getHeader: (name) => NextResponse.headers?.get(name),
+      setHeader: (name, value) => NextResponse.headers?.set(name, value),
+    },
+    authOptions
+  );
+  if (!session) {
+    return Response.json(
+      { message: "You must be logged in." },
+      { status: 401 }
+    );
+  } else if (session && session?.user?.role !== "admin") {
+    return Response.json({ message: "Forbidden request" }, { status: 403 });
+  }
   try {
     await connectDB;
-    const categories = await Category.find({});
+    const categories = await Category.find();
     console.log(categories);
     return new Response(JSON.stringify({ success: true, categories }), {
       status: 200,
     });
   } catch (error) {
-    return new Response(
-      JSON.stringify({ success: false, message: error.message }),
+    return Response.json(
+      { success: false, message: error.message },
       { status: 500 }
     );
   }
