6e499779d6b1ba54dce7ae24419bca96a55116b2
Bright <brightagu2@gmail.com>
2024-09-10 02:32:12 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -492,6 +492,29 @@ const page = () => {
           </p>
         </div>
       </footer>
+      <div
+        style={{
+          width: "70px",
+          height: "70px",
+          borderRadius: "50%",
+          margin: "30px",
+          zIndex: "999",
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          color: "white",
+        }}
+      >
+        <a
+          href="https://chat.whatsapp.com/LotGhr8csOs3pAYbkK14Eb"
+          target="_blank"
+        >
+          <img
+            style={{ width: "100%", height: "100%" }}
+            src="/img/telegram.png"
+          />
+        </a>
+      </div>
     </div>
   );
 };
