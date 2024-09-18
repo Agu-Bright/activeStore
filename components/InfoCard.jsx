198519363bd4b9871483948264194dc603e0e305
Bright <brightagu2@gmail.com>
2024-09-18 09:36:02 +0100

    update

--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -1,13 +1,27 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/free-mode";
 import { FreeMode } from "swiper/modules";
 import { Box, Stack } from "@mui/material";
 import { useSession } from "next-auth/react";
+import axios from "axios";
+import { Data } from "@react-google-maps/api";
 const InfoCards = ({ summary }) => {
   const { data: session, status } = useSession();
+  const [dashboard, setDashboard] = useState();
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("/api/get-dashboard");
+        console.log("dashboard", data);
+        setDashboard(data?.dashboard);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
   return (
     <>
       <Stack
@@ -21,10 +35,10 @@ const InfoCards = ({ summary }) => {
           <div className="col-lg-3 col-md-3 col-sm-6">
             <div className="ui-item bg-success">
               <div className="left">
-                {session?.user?.role === "admin" && <p>Orders</p>}
+                {session?.user?.role === "admin" && <p>Today's Orders</p>}
 
                 {session?.user?.role === "admin" && (
-                  <h4>{summary?.totalReservations || "0"}</h4>
+                  <h4>{dashboard?.orders || "0"}</h4>
                 )}
               </div>
               {/* <div className="right">
@@ -37,10 +51,10 @@ const InfoCards = ({ summary }) => {
               <div className="left">
                 {session?.user?.role === "admin" && <p>Todays Deposit</p>}
                 {session?.user?.role === "admin" && (
-                  <h4>{summary?.totalRestaurants || "0"}</h4>
+                  <h4>{dashboard?.deposits || "0"}</h4>
                 )}
                 {session?.user?.role === "sub-admin" && (
-                  <h4>{summary?.todaysReservations || "0"}</h4>
+                  <h4>{dashboard?.deposits || "0"}</h4>
                 )}
                 {/* <p>Listing Views</p> */}
               </div>
@@ -55,10 +69,10 @@ const InfoCards = ({ summary }) => {
                 {session?.user?.role === "admin" && <p>Logs</p>}
                 {session?.user?.role === "sub-admin" && <p>Logs</p>}
                 {session?.user?.role === "admin" && (
-                  <h4>{summary?.totalSubsc || "0"}</h4>
+                  <h4>{dashboard?.logs || "0"}</h4>
                 )}
                 {session?.user?.role === "sub-admin" && (
-                  <h4>{summary?.revCount || "0"}</h4>
+                  <h4>{dashboard?.logs || "0"}</h4>
                 )}
               </div>
               {/* <div className="right">
@@ -73,7 +87,7 @@ const InfoCards = ({ summary }) => {
                 <div className="left">
                   {session?.user?.role === "admin" && <p>Users</p>}
                   {session?.user?.role === "admin" && (
-                    <h4>{summary?.totalUsers || "0"}</h4>
+                    <h4>{dashboard?.users || "0"}</h4>
                   )}
 
                   {/* <p>Bookmarked</p> */}
@@ -107,9 +121,9 @@ const InfoCards = ({ summary }) => {
             >
               <div className="ui-item bg-success">
                 <div className="left">
-                  {session?.user?.role === "admin" && <p>Deposits</p>}
+                  {session?.user?.role === "admin" && <p>Today's Orders</p>}
                   {session?.user?.role === "admin" && (
-                    <h4>{summary?.restaurantRequests || "NGN 0"}</h4>
+                    <h4>{dashboard?.orders || "NGN 0"}</h4>
                   )}
                 </div>
                 <div className="right">
@@ -125,12 +139,12 @@ const InfoCards = ({ summary }) => {
             >
               <div className="ui-item bg-warning">
                 <div className="left">
-                  {session?.user?.role === "admin" && <p> Todays Deposits</p>}
+                  {session?.user?.role === "admin" && <p> Today's Deposits</p>}
                   {session?.user?.role === "admin" && (
-                    <h4>{summary?.totalRestaurants || "0"}</h4>
+                    <h4>{dashboard?.deposits || "0"}</h4>
                   )}
                   {session?.user?.role === "sub-admin" && (
-                    <h4>{summary?.todaysReservations || "0"}</h4>
+                    <h4>{dashboard?.deposits || "0"}</h4>
                   )}
                   {/* <p>Listing Views</p> */}
                 </div>
@@ -145,9 +159,9 @@ const InfoCards = ({ summary }) => {
               <div className="col-lg-3 col-md-3 col-sm-6">
                 <div className="ui-item bg-active">
                   <div className="left">
-                    {session?.user?.role === "admin" && <p>Empty Logs</p>}
+                    {session?.user?.role === "admin" && <p>Logs</p>}
                     {session?.user?.role === "admin" && (
-                      <h4>{summary?.totalSubsc || "0"}</h4>
+                      <h4>{dashboard?.logs || "0"}</h4>
                     )}
                   </div>
                   <div className="right">
@@ -165,30 +179,7 @@ const InfoCards = ({ summary }) => {
                     <div className="left">
                       {session?.user?.role === "admin" && <p>Users</p>}
                       {session?.user?.role === "admin" && (
-                        <h4>{summary?.totalUsers || "0"}</h4>
-                      )}
-
-                      {/* <p>Bookmarked</p> */}
-                    </div>
-                    <div className="right">
-                      <i className="fa fa-heart-o"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          )}
-          {session?.user?.role === "admin" && (
-            <SwiperSlide>
-              <div>
-                <div className="col-lg-3 col-md-3 col-sm-6">
-                  <div className="ui-item bg-dark">
-                    <div className="left">
-                      {session?.user?.role === "admin" && (
-                        <p>Customer Service</p>
-                      )}
-                      {session?.user?.role === "admin" && (
-                        <h4>{summary?.totalUsers || "0"}</h4>
+                        <h4>{dashboard?.users || "0"}</h4>
                       )}
 
                       {/* <p>Bookmarked</p> */}
